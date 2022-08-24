06572630f0b33bf521e618f7086671e02cef3c9b
GeoMorttis <rodrigues1975@outlook.com>
2022-08-24 15:40:40 -0300

    Atualização do script com exercício prático do final da aula.

--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse('<h1>Hello {} de {} anos.</h1>'.format(nome, idade))
+
+def soma(request, num1, num2):
+    resul = num1 + num2
+    return HttpResponse('<h1>A soma de {} mais {} é igual a {}.'.format(num1, num2, resul))
